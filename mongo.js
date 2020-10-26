bd63c4ca4bca950cd40975db58dc3bd95485a2e2
Vinicios Psidonik <vinicios.psidonik@compasso.com.br>
2020-10-26 11:22:03 -0300

    .

--- mongo.js
+++ mongo.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
+
 const mongoURI = process.env.MONGO
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
